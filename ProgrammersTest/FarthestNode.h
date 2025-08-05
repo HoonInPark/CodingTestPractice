b9d613875f55e4260a28c3565bfdd0349ca31ddd
HoonInPark <hooninpark@gmail.com>
2025-08-05 09:55:47 +0900

    path finder impl

--- ProgrammersTest/FarthestNode.h
+++ ProgrammersTest/FarthestNode.h
@@ -1,11 +1,40 @@
 #pragma once
 
+/*
+* https://school.programmers.co.kr/learn/courses/30/lessons/49189
+*/
+
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 int solution(int n, vector<vector<int>> edge) 
 {
+	int answer = 0;
 
+	// branch-leaf 형태의 map이 만들 것임.
+	unordered_map<int, unordered_set<int>> PathFinder;
+	for (int i = 1; i < n + 1; ++i)
+		PathFinder.insert(make_pair(i, unordered_set<int>()));
+
+	for (int j = 1; j < n + 1; ++j)
+	{
+		for (int k = 0; k < edge.size(); ++k)
+		{
+			if (edge[k][0] == j || edge[k][1] == j)
+			{
+				(*PathFinder.find(j)).second.insert(k);
+			}
+		}
+	}
+
+	for (int l = 2; l < n + 1; ++l)
+	{
+		
+	}
+
+	return answer;
 }
--- ProgrammersTest/ProgrammersTest.cpp
+++ ProgrammersTest/ProgrammersTest.cpp
@@ -6,9 +6,28 @@
 //#include "PseudoCantorBitArray.h"
 //#include "EnglishWordChain.h"
 //#include "PrimeNumGen.h"
+#include "FarthestNode.h"
 
 int main()
 {
+	vector<vector<int>> test;
+	test.push_back({ 3, 6 }); // 0
+	test.push_back({ 4, 3 }); // 1
+	test.push_back({ 3, 2 }); // 2
+	test.push_back({ 1, 3 }); // 3
+	test.push_back({ 1, 2 }); // 4
+	test.push_back({ 2, 4 }); // 5
+	test.push_back({ 5, 2 }); // 6
 
+	solution(6, test);
 }
 
+/*
+[3, 6], 
+[4, 3], 
+[3, 2], 
+[1, 3], 
+[1, 2], 
+[2, 4], 
+[5, 2]
+*/
