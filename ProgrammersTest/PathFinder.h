3ae29b5a6c2ebb97949f2df2d3e976325a83eab5
HoonInPark <hooninpark@gmail.com>
2025-08-18 14:06:18 +0900

    find appropriate for making bin tree impl

--- ProgrammersTest/PathFinder.h
+++ ProgrammersTest/PathFinder.h
@@ -7,16 +7,18 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
+#include <set>
 
 using namespace std;
 
-bool Comp(const vector<int>&, const vector<int>&);
+bool CmpOfVec(const vector<int>&, const vector<int>&);
 
 struct Node
 {
 	Node(int _InNodeNum)
 		: m_NodeNum(_InNodeNum)
-	{}
+	{
+	}
 
 	int m_NodeNum = -1;
 	Node* m_pChild_L = nullptr;
@@ -34,19 +36,32 @@ vector<vector<int>> solution(vector<vector<int>> nodeinfo)
 		nodeinfo[i].push_back(i + 1); // nodeinfo[i][2]는 원래 해당 노드의 인덱스 값. 재할당 필요 없지롱 쿠쿠루삥뽕
 
 	// 이렇게 하면 맨 위에 있는 노드 순으로, 왼쪽에 있는 노드 순으로 정렬된다. 
-	sort(nodeinfo.begin(), nodeinfo.end(), Comp);
+	sort(nodeinfo.begin(), nodeinfo.end(), CmpOfVec);
 	const auto& NodeInfos = nodeinfo;
 
 	// 앞에서부터 하나씩 꺼내면서 좌표평면을 분할하는 수직선을 만든다고 생각하자. 
 	// 위에 있는 노드를 기준으로 그은 수직선은 서브트리 모두에게 영향을 미친다.
 	int CurHeight = -1;
-	int CurIdx = 3;
+	int CurIdx = 3; // 맨 위에 세개(인덱스 0, 1, 2)는 연결할 필요가 없으니 패스
 
 	vector<int> NodeIdxBuf; // 이거 원래 인덱스 아니고 정렬된 기준이니 주의 요망.
-	NodeMap.insert(make_pair(2, new Node(NodeInfos[2][2]))); // 루트.
+
+	auto CmpOfSet = [&NodeInfos](int _InFormer, int _InLater)
+		{
+			return NodeInfos[_InFormer][0] < NodeInfos[_InLater][0];
+		};
+	set<int, decltype(CmpOfSet)> NodeIdxRounded(CmpOfSet);
+	NodeIdxRounded.insert(0);
+	NodeIdxRounded.insert(1);
+	NodeIdxRounded.insert(2);
+
+	NodeMap.insert(make_pair(2, new Node(NodeInfos[2][2]))); // 루트. key는 정렬된 기준 인덱스.
 
 	while (CurIdx < NodeInfos.size())
 	{
+		for (const auto NodeIdx : NodeIdxBuf)
+			NodeIdxRounded.insert(NodeIdx);
+
 		NodeIdxBuf.clear();
 
 		do
@@ -58,15 +73,17 @@ vector<vector<int>> solution(vector<vector<int>> nodeinfo)
 
 		} while (CurIdx < NodeInfos.size() && CurHeight <= NodeInfos[CurIdx][1]);
 
-		// 자기 보다 상위에 있는 노드들 중 자신으로부터 양 옆에 있는 가장 가까운 노드를 기준으로 수직선을 긋고 
-		// 그 두 노드 중 층이 낮은 녀석에게 연결하면 됨.
-		// 위의 do while로 얻어낸 배열은 부모 노드에 연결되지 않은 (이제 다음 for문에서 연결해야 하는) 배열임.
-		for (int k = 0; k < NodeIdxBuf.size(); k++)
+		for (int i = 0; i < NodeIdxBuf.size(); ++i)
 		{
-			const int& NodeIdxInThisDepth = NodeIdxBuf[k];
-			for (int l = 0; l < NodeIdxInThisDepth; l++)
+			const int X = NodeInfos[NodeIdxBuf[i]][0];
+			for (auto iter = NodeIdxRounded.begin(); iter != prev(NodeIdxRounded.end()); iter++)
 			{
-				//if (NodeInfos[NodeIdxInThisDepth][0] < )
+				int FormerX = NodeInfos[*iter][0];
+				int LaterX = NodeInfos[*next(iter)][0];
+				if (FormerX < X && X < LaterX)
+				{
+					break;
+				}
 			}
 		}
 	}
@@ -74,7 +91,7 @@ vector<vector<int>> solution(vector<vector<int>> nodeinfo)
 	return nodeinfo;
 }
 
-bool Comp(const vector<int>& _InFormer, const vector<int>& _InLater)
+bool CmpOfVec(const vector<int>& _InFormer, const vector<int>& _InLater)
 {
 	if (_InFormer[1] == _InLater[1])
 		return _InFormer[0] < _InLater[0];
