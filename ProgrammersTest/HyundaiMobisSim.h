f675efc4ace5e168397cf2434a5d012d3ac51722
HoonInPark <hooninpark@gmail.com>
2025-08-20 22:59:45 +0900

    structure comment

--- ProgrammersTest/HyundaiMobisSim.h
+++ ProgrammersTest/HyundaiMobisSim.h
@@ -6,6 +6,7 @@
 
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
@@ -16,13 +17,14 @@ struct Node
 	{
 	}
 
-	vector<Node*> m_NextNodes;
-	int m_NodeNum;
+	unordered_set<Node*> m_NextNodes;
+	int m_NodeNum = -1;
+	bool m_bIsVisited = false;
 };
 
 int solution(int n, vector<vector<int>> roads)
 {
-	int answer = 0;
+	int RetNum = 0;
 
 	unordered_map<int, Node*> MapOfNodes;
 
@@ -35,13 +37,24 @@ int solution(int n, vector<vector<int>> roads)
 		if (!pNode_1)
 			MapOfNodes[road[1]] = new Node(road[1]);
 		
-		MapOfNodes[road[0]]->m_NextNodes.push_back(MapOfNodes[road[1]]);
+		MapOfNodes[road[0]]->m_NextNodes.insert(MapOfNodes[road[1]]);
 	}
 
 	auto pRoot = MapOfNodes[1];
 
+	/*
+	* 가장 먼저, 1에서 출발하여 NextNodes.empty()인 노드에 도달할 때까지 
+	* m_bIsVisited = true를 마킹하며 계속 순회.
+	* 끝에 다다르면, RetNum++;
+	* MapOfNodes의 값 중 m_bIsVisited == false인 것부터 출발하여 다시 같은 방식으로 순회.
+	* 근데 만약 MapOfNodes 값 모두 m_bIsVisited == true면 모두 종료.
+	* 근데, 순회가 하나의 노드만 m_bIsVisited = true 처리하고 끝날 수 있음.
+	*/
+
+	
+
 	for (const auto NodePair : MapOfNodes)
 		delete NodePair.second;
 
-	return answer;
+	return RetNum;
 }
