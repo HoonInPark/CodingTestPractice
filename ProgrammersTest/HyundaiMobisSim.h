d53b1fae274803afdf49dacefbc3924d04c25120
HoonInPark <hooninpark@gmail.com>
2025-08-21 10:27:10 +0900

    just idea, not solved

--- ProgrammersTest/HyundaiMobisSim.h
+++ ProgrammersTest/HyundaiMobisSim.h
@@ -16,9 +16,11 @@ struct Node
 	{
 	}
 
-	vector<Node*> m_NextNodes;
 	int m_NodeNum = -1;
 	bool m_bIsVisited = false;
+	
+	vector<Node*> m_PrevNodes;
+	vector<Node*> m_NextNodes;
 };
 
 int solution(int n, vector<vector<int>> roads)
@@ -27,24 +29,51 @@ int solution(int n, vector<vector<int>> roads)
 
 	unordered_map<int, Node*> MapOfNodes;
 
+	for (int i = 1; i <= n; ++i)
+		MapOfNodes[i] = new Node(i);
 	for (const auto& road : roads)
 	{
-		auto pNode_0 = MapOfNodes[road[0]];
-		auto pNode_1 = MapOfNodes[road[1]];
-		if (!pNode_0)
-			MapOfNodes[road[0]] = new Node(road[0]);
-		if (!pNode_1)
-			MapOfNodes[road[1]] = new Node(road[1]);
-		
+		MapOfNodes[road[1]]->m_PrevNodes.push_back(MapOfNodes[road[0]]);
 		MapOfNodes[road[0]]->m_NextNodes.push_back(MapOfNodes[road[1]]);
 	}
 
 	auto pNode = MapOfNodes[1];
 
-
+	// do something
+	/*
+	* 일단 생각하고 있는 방법은...
+	* 일반화된 규칙을 찾는 건 불가능하다. 다른 사람의 풀이를 봐도 가능한 모든 경우의 수를 고려한다. 
+	* 따라서, pNode로부터 시작하여서 true == m_NextNodes.empty()일 때까지 다음 노드를 찾아 
+	* m_bIsVisited를 true로 바꾸면서 순회하고, 
+	* 한 줄 순회가 끝나면 MapOfNodes 중 아직 순회하지 않은 노드를 찾는다. ... (1)
+	* 근데 찾은 그 노드가 m_PrevNodes 중 아직 순회하지 않은 노드(즉, false == m_NextNodes.empty())가 있으면 
+	* 찾아 올라간다.
+	* 이걸 반복하며 순회하지 않은 노드 중 첫번째부터 다시 위 과정을 반복한다. 
+	* 이렇게 순회 방법의 케이스 하나가 나오는 것임. 
+	* 물론 순회하면서 (1)에서 '빠른 이동' 횟수를 세야 한다. 
+	* 모든 경우의 수를 고려하며 이 횟수가 가장 적은 걸 선택하면 되는 문제.
+	*/
 
 	for (const auto NodePair : MapOfNodes)
 		delete NodePair.second;
 
 	return RetNum;
 }
+
+/*
+#include "HyundaiMobisSim.h"
+
+int main()
+{
+	vector<vector<int>> test;
+	test.push_back({ 1, 2 });
+	test.push_back({ 2, 3 });
+	test.push_back({ 3, 4 });
+	test.push_back({ 4, 5 });
+	test.push_back({ 3, 6 });
+	test.push_back({ 1, 7 });
+	test.push_back({ 7, 4 });
+
+	solution(7, test); // 정답은 1
+}
+*/
--- ProgrammersTest/ProgrammersTest.cpp
+++ ProgrammersTest/ProgrammersTest.cpp
@@ -11,36 +11,18 @@
 //#include "ElectricalGridDivide.h"
 //#include "HanoiTower.h"
 //#include "PathFinder.h"
-#include "HyundaiMobisSim.h"
+//#include "HyundaiMobisSim.h"
 
 int main()
 {
-	vector<vector<int>> test;
-	test.push_back({1, 2});
-	test.push_back({1, 3});
-	test.push_back({1, 4});
-	test.push_back({2, 5});
-	test.push_back({4, 5});
-	test.push_back({5, 6});
-	test.push_back({5, 7});
-	test.push_back({6, 9});
-	test.push_back({7, 9});
-	test.push_back({5, 8});
-
-	solution(9, test); // 정답은 3
 }
 
 /*
 [
-[1, 2], 
-[1, 3], 
-[1, 4], 
-[2, 5], 
-[4, 5], 
-[5, 6], 
-[5, 7], 
-[6, 9], 
-[7, 9], 
-[5, 8]
-]
-*/
+[1, 2],
+[2, 3],
+[3, 4],
+[4, 5],
+[3, 6],
+[1, 7],
+[7, 4]]*/
