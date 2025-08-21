26a8949d78d0db1dcf62612ae011c25276151547
HoonInPark <hooninpark@gmail.com>
2025-08-21 14:08:54 +0900

    modified little bit

--- ProgrammersTest/FillThePuzzle.h
+++ ProgrammersTest/FillThePuzzle.h
@@ -14,25 +14,31 @@ int solution(vector<vector<int>> game_board, vector<vector<int>> table)
 	{
 		for (int j = 0; j < table[i].size(); ++j) // j는 x좌표
 		{
-			if (table[i][j])
+			if (!table[i][j]) continue;
+
+			queue<vector<int>> YX;
+			YX.push({ i, j });
+
+			// 여기서 너비 우선 탐색.
+			// 이 while문이 끝나면 하나의 퍼즐 조각을 모두 순회함.
+			while (!YX.empty())
 			{
-				queue<vector<int>> YX;
-				YX.push({ i, j });
+				vector<int> FrontPt = YX.front();
+				YX.pop();
 
-				// 여기서 너비 우선 탐색.
-				// 이 while문이 끝나면 하나의 퍼즐 조각을 모두 순회함.
-				while (!YX.empty())
-				{
-					vector<int> FrontPt = YX.front();
-					YX.pop();
+				table[FrontPt[0]][FrontPt[1]] = 0; // 순회했으니 0으로 표기.
 
-					table[FrontPt[0]][FrontPt[1]] = 0; // 순회했으니 0으로 표기.
+				if (FrontPt[0] - 1 > -1 && table[FrontPt[0] - 1][FrontPt[1]])
+					YX.push({ FrontPt[0] - 1, FrontPt[1] });
 
-					if (table[FrontPt[0] - 1][FrontPt[1]]) YX.push({ FrontPt[0] - 1, FrontPt[1] });
-					if (table[FrontPt[0] + 1][FrontPt[1]]) YX.push({ FrontPt[0] + 1, FrontPt[1] });
-					if (table[FrontPt[0]][FrontPt[1] - 1]) YX.push({ FrontPt[0], FrontPt[1] - 1 });
-					if (table[FrontPt[0]][FrontPt[1] + 1]) YX.push({ FrontPt[0], FrontPt[1] + 1 });
-				}
+				if (FrontPt[0] + 1 < table.size() && table[FrontPt[0] + 1][FrontPt[1]])
+					YX.push({ FrontPt[0] + 1, FrontPt[1] });
+
+				if (FrontPt[1] - 1 > -1 && table[FrontPt[0]][FrontPt[1] - 1])
+					YX.push({ FrontPt[0], FrontPt[1] - 1 });
+
+				if (FrontPt[1] + 1 < table.size() && table[FrontPt[0]][FrontPt[1] + 1])
+					YX.push({ FrontPt[0], FrontPt[1] + 1 });
 			}
 		}
 	}
