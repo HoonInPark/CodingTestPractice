03442d6db02139dd9941c6e542aaac17cc6c2ffd
HoonInPark <hooninpark@gmail.com>
2025-08-21 13:48:52 +0900

    most simple logic impl

--- ProgrammersTest/FillThePuzzle.h
+++ ProgrammersTest/FillThePuzzle.h
@@ -1,28 +1,41 @@
 #pragma once
 
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-int solution(vector<vector<int>> game_board, vector<vector<int>> table) 
+int solution(vector<vector<int>> game_board, vector<vector<int>> table)
 {
-    int answer = -1;
+	int answer = -1;
 
-    // 일단 퍼즐 조각이 맞아 들어가는지부터 확인. 그게 최적의 경우인지는 다음으로 생각합시다.
-    for (int i = 0; i < table.size(); ++i) // i는 y좌표
-    {
-        for (int j = 0; j < table[i].size(); ++j) // j는 x좌표
-        {
-            if (table[i][j])
-            {
-                // 여기서 너비 우선 탐색.
-                while (true)
-                { 
-                    
-                }
-            }
-        }
-    }
+	// 일단 퍼즐 조각이 맞아 들어가는지부터 확인. 그게 최적의 경우인지는 다음으로 생각합시다.
+	for (int i = 0; i < table.size(); ++i) // i는 y좌표
+	{
+		for (int j = 0; j < table[i].size(); ++j) // j는 x좌표
+		{
+			if (table[i][j])
+			{
+				queue<vector<int>> YX;
+				YX.push({ i, j });
 
-    return answer;
+				// 여기서 너비 우선 탐색.
+				// 이 while문이 끝나면 하나의 퍼즐 조각을 모두 순회함.
+				while (!YX.empty())
+				{
+					vector<int> FrontPt = YX.front();
+					YX.pop();
+
+					table[FrontPt[0]][FrontPt[1]] = 0; // 순회했으니 0으로 표기.
+
+					if (table[FrontPt[0] - 1][FrontPt[1]]) YX.push({ FrontPt[0] - 1, FrontPt[1] });
+					if (table[FrontPt[0] + 1][FrontPt[1]]) YX.push({ FrontPt[0] + 1, FrontPt[1] });
+					if (table[FrontPt[0]][FrontPt[1] - 1]) YX.push({ FrontPt[0], FrontPt[1] - 1 });
+					if (table[FrontPt[0]][FrontPt[1] + 1]) YX.push({ FrontPt[0], FrontPt[1] + 1 });
+				}
+			}
+		}
+	}
+
+	return answer;
 }
