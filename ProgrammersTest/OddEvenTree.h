300e0a0883d5f6a18c54c385f23b47c512b01302
HoonInPark <hooninpark@gmail.com>
2025-08-22 12:11:06 +0900

    debugging issue solved

--- ProgrammersTest/OddEvenTree.h
+++ ProgrammersTest/OddEvenTree.h
@@ -1,11 +1,38 @@
 #pragma once
-#include <string>
+
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-vector<int> solution(vector<int> nodes, vector<vector<int>> edges) 
+struct Node
 {
-    vector<int> answer;
-    return answer;
+	Node(int _InNodeNum)
+		: m_NodeNum(_InNodeNum)
+	{
+	}
+
+	int m_NodeNum = 0;
+	vector<Node*> m_Neighbors;
+};
+
+vector<int> solution(vector<int> nodes, vector<vector<int>> edges)
+{
+	unordered_map<int, Node*> MapOfNodes;
+
+	for (const auto NodeNum : nodes)
+	{
+		auto pNode = new Node(NodeNum);
+		MapOfNodes[NodeNum] = pNode;
+	}
+
+	for (const auto& Edge : edges)
+	{
+		MapOfNodes[Edge[0]]->m_Neighbors.push_back(MapOfNodes[Edge[1]]);
+		MapOfNodes[Edge[1]]->m_Neighbors.push_back(MapOfNodes[Edge[0]]);
+	}
+
+
+
+	return vector<int>();
 }
