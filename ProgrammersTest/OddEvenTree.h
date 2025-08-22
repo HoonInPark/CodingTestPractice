ef7efa98f06269c67a37bad10f37d5a2fb0e3f51
HoonInPark <hooninpark@gmail.com>
2025-08-22 15:58:48 +0900

    fixing minor bug... probably.. must be...

--- ProgrammersTest/OddEvenTree.h
+++ ProgrammersTest/OddEvenTree.h
@@ -1,7 +1,12 @@
 #pragma once
 
+/*
+* https://school.programmers.co.kr/learn/courses/30/lessons/388354
+*/
+
 #include <vector>
 #include <unordered_map>
+#include <queue>
 
 using namespace std;
 
@@ -13,6 +18,9 @@ struct Node
 	}
 
 	int m_NodeNum = 0;
+	int m_ChildrenNum = 0;
+
+	bool m_bIsChecked = false;
 	vector<Node*> m_Neighbors;
 };
 
@@ -32,7 +40,78 @@ vector<int> solution(vector<int> nodes, vector<vector<int>> edges)
 		MapOfNodes[Edge[1]]->m_Neighbors.push_back(MapOfNodes[Edge[0]]);
 	}
 
+	// 자식 노드 수 카운트 
+	for (auto& NodePair : MapOfNodes)
+	{
+		queue<Node*> Q;
+		Q.push(NodePair.second);
+		NodePair.second->m_bIsChecked = true;
 
+		while (!Q.empty())
+		{
+			auto pNode = Q.front();
+			Q.pop();
+			////////////////////////////////////////////////////////////////////////////
+			++NodePair.second->m_ChildrenNum;
+			////////////////////////////////////////////////////////////////////////////
 
-	return vector<int>();
+			for (const auto pNeighbor : pNode->m_Neighbors)
+			{
+				if (pNeighbor->m_bIsChecked) continue;
+				Q.push(pNeighbor);
+				pNeighbor->m_bIsChecked = true;
+			}
+		}
+
+		for (auto& NodePairForReset : MapOfNodes)
+			NodePairForReset.second->m_bIsChecked = false;
+
+		--NodePair.second->m_ChildrenNum;
+	}
+
+	int OddEvenNumTree = 0;
+	int ReverseOddEvenNumTree = 0;
+
+	for (auto& NodePair : MapOfNodes)
+	{
+		int OddEvenNum = 0;
+		int ReverseOddEvenNum = 0;
+
+		queue<Node*> Q;
+		Q.push(NodePair.second);
+		NodePair.second->m_bIsChecked = true;
+
+		while (!Q.empty())
+		{
+			auto pNode = Q.front();
+			Q.pop();
+
+			////////////////////////////////////////////////////////////////////////////
+			if (pNode->m_NodeNum % 2 == pNode->m_ChildrenNum % 2)
+				++OddEvenNum;
+			else
+				++ReverseOddEvenNum;
+			////////////////////////////////////////////////////////////////////////////
+
+			for (const auto pNeighbor : pNode->m_Neighbors)
+			{
+				if (pNeighbor->m_bIsChecked) continue;
+				Q.push(pNeighbor);
+				pNeighbor->m_bIsChecked = true;
+			}
+		}
+
+		for (auto& NodePairForReset : MapOfNodes)
+			NodePairForReset.second->m_bIsChecked = false;
+
+		if (0 == OddEvenNum * ReverseOddEvenNum)
+		{
+			if (OddEvenNum)
+				++OddEvenNumTree;
+			else if (ReverseOddEvenNum)
+				++ReverseOddEvenNumTree;
+		}
+	}
+
+	return { OddEvenNumTree , ReverseOddEvenNumTree };
 }
