09830166d5b1f4210b2f082f943a6b9f7078053f
HoonInPark <hooninpark@gmail.com>
2025-08-26 13:31:22 +0900

    correct

--- ProgrammersTest/OddEvenTree.h
+++ ProgrammersTest/OddEvenTree.h
@@ -1,12 +1,10 @@
 #pragma once
-
 /*
 * https://school.programmers.co.kr/learn/courses/30/lessons/388354
 */
 
 #include <vector>
 #include <unordered_map>
-#include <unordered_set>
 #include <queue>
 
 using namespace std;
@@ -43,24 +41,23 @@ vector<int> solution(vector<int> nodes, vector<vector<int>> edges)
 	int ResNum_1 = 0;
 	int ResNum_2 = 0;
 
-	int OddEvenNum = 0;
-	int RevOddEvenNum = 0;
+	int OddEvenNum;
+	int RevOddEvenNum;
 
 	// 자식 노드 수 카운트 
 	for (const auto& NodePair : MapOfNodes)
 	{
+		if (NodePair.second->m_bIsChecked) continue;
+
 		OddEvenNum = 0;
 		RevOddEvenNum = 0;
 
 		Dfs(NodePair.second, OddEvenNum, RevOddEvenNum);
 
-		if (0 == OddEvenNum * RevOddEvenNum)
-		{
-			if (OddEvenNum)
-				++ResNum_1;
-			else if (RevOddEvenNum)
-				++ResNum_2;
-		}
+		if (1 == OddEvenNum)
+			++ResNum_1;
+		if (1 == RevOddEvenNum)
+			++ResNum_2;
 	}
 
 	for (auto& NodePair : MapOfNodes)
@@ -71,22 +68,16 @@ vector<int> solution(vector<int> nodes, vector<vector<int>> edges)
 
 void Dfs(Node* _pInNode, int& _InOddEvenNum, int& _InRevOddEvenNum)
 {
-	int ChildrenNum = 0;
 	_pInNode->m_bIsChecked = true;
 
 	for (auto pNode : _pInNode->m_Neighbors)
 	{
 		if (pNode->m_bIsChecked) continue;
-
-		++ChildrenNum;
 		Dfs(pNode, _InOddEvenNum, _InRevOddEvenNum);
 	}
 
-	_pInNode->m_bIsChecked = false;
-
-	//cout << "NodeNum : " << _pInNode->m_NodeNum << " ChildrenNum : " << ChildrenNum << endl;
-	if (_pInNode->m_NodeNum % 2 == ChildrenNum % 2) // 지금 인수로 들어온 _pInNode가 홀수 노드 혹은 짝수 노드인 경우
+	if (_pInNode->m_NodeNum % 2 == _pInNode->m_Neighbors.size() % 2)
 		++_InOddEvenNum;
-	else // 지금 인수로 들어온 _pInNode가 역홀수 노드 혹은 역짝수 노드인 경우
+	else
 		++_InRevOddEvenNum;
 }
