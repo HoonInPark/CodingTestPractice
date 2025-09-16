48b623e7b8bd02ea3404911bdfe3f331040aff43
HoonInPark <hooninpark@gmail.com>
2025-09-16 22:36:57 +0900

    PerfectCrime header added

---
+++ ProgrammersTest/PerfectCrime.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/*
+* https://school.programmers.co.kr/learn/courses/30/lessons/389480
+*/
+
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> info, int n, int m) 
+{
+    int answer = 0;
+    return answer;
+}
