3313bcb7656358a4b68cad51bbea2fb74b8d617e
HoonInPark <hooninpark@gmail.com>
2025-09-18 15:03:47 +0900

    good start....

--- ProgrammersTest/PerfectCrime.h
+++ ProgrammersTest/PerfectCrime.h
@@ -5,24 +5,41 @@
 */
 
 #include <vector>
+#include <algorithm> 
 
 using namespace std;
 
+bool cmp(const vector<int>& _InFormer, const vector<int>& _InLater);
+
 int solution(vector<vector<int>> info, int n, int m)
 {
-    
+    sort(info.begin(), info.end(), cmp);
+
+    for (int a = 0; a < info.size(); ++a)
+    {
+
+    }
     
     return -1;
 }
+
+bool cmp(const vector<int>& _InFormer, const vector<int>& _InLater)
+{
+    return _InFormer[0] < _InLater[0];
+}
+
 
 /*
 * 1. 모두 훔쳐야 한다.
 * 2. 경찰에 잡히면 안된다. 
 * 3. 잡히는 조건은 흔적 총 합이 A는 n개 이상, B는 m개 이상일 때.
-* 4. 반환값은 A가 최소로 남기는 흔적 개수
+* 4. 반환값은 A가 최소로 남기는 흔적 수
 * 
 * A는 최소가 되게 훔치고, B는 붙잡히면 안됨. 
 * 이 로직으로 해도 붙잡히면, -1 반환.
+* 
+* 1 2 2
+* 2 3 1
 * 
 */
 
