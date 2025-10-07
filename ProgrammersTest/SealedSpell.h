0cecc1f03dbc30efe75d82adc51c0d5bca4d26f7
HoonInPark <hooninpark@gmail.com>
2025-10-07 17:43:42 +0900

    fixed

--- ProgrammersTest/SealedSpell.h
+++ ProgrammersTest/SealedSpell.h
@@ -10,29 +10,37 @@
 
 using namespace std;
 
-long long Pow26(int exp);
+long long Pow26(long long exp);
 
 string solution(long long n, vector<string> bans)  // 1부터 시작하는 카운팅
 {
     string ResStr;
 
+    vector<int> BanStrIdxs;
+
     // 전부 0으로부터 시작하는 수로 변환
     for (const auto& BanStr : bans)
     {
-        for (int i = 0; i < BanStr.size(); ++i)
+        long long ResNum = 0;
+        for (long long i = 0; i < BanStr.size() - 1; ++i)
         {
-
+            ResNum += Pow26(BanStr.size() - 1 - i)* (BanStr[i] - 96);
         }
+
+        ResNum += BanStr[BanStr.size() - 1] - 97;
+        BanStrIdxs.push_back(ResNum);
     }
+
+
 
     return ResStr;
 }
 
-long long Pow26(int exp)
+long long Pow26(long long exp)
 {
     long long res = 1;
 
-    for (int i = 0; i < exp; ++i)
+    for (long long i = 0; i < exp; ++i)
         res *= 26;
 
     return res;
