84c39eedb790a3ce561ccfce87245a4c95cff128
HoonInPark <hooninpark@gmail.com>
2025-10-08 15:34:28 +0900

    with status vector, it would be brure force....

--- ProgrammersTest/ForkliftAndCrane.h
+++ ProgrammersTest/ForkliftAndCrane.h
@@ -16,30 +16,7 @@ int solution(vector<string> storage, vector<string> requests)
     int m = storage[0].size();
     int n = storage.size();
 
-    vector<vector<bool>> Status;
-    Status.push_back(vector<bool>(m, true));
-    for (int i = 0; i < n - 2; ++i)
-    {
-        vector<bool> Mid(m, false);
 
-        Mid[0] = true;
-        Mid[m - 1] = true;
-
-        Status.push_back(Mid);
-    }
-    Status.push_back(vector<bool>(m, true));
-
-    for (int j = 0; j < requests.size(); ++j)
-    {
-        if (requests[j].size() == 1) // 지게차
-        {
-
-        }
-        else // 크레인 
-        {
-
-        }
-    }
 
     return answer;
 }
