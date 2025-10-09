a2b7f9e890738287c36107658a7088f48cc3d603
HoonInPark <hooninpark@gmail.com>
2025-10-09 14:04:37 +0900

    forklift logic impl

--- ProgrammersTest/ForkliftAndCrane.h
+++ ProgrammersTest/ForkliftAndCrane.h
@@ -10,57 +10,91 @@
 
 using namespace std;
 
-int solution(vector<string> storage, vector<string> requests) 
+int solution(vector<string> storage, vector<string> requests)
 {
-    int answer = 0;
+	int answer = 0;
 
-    int m = storage[0].size();
-    int n = storage.size();
+	const int m = storage[0].size();
+	const int n = storage.size();
 
-    unordered_set<string> OutsideStuffs;
+	vector<vector<bool>> Status(n, vector<bool>(m, true));
+	unordered_set<string> OutsideStuffSet;
 
-    for (int i = 0; i < m; ++i)
-    {
-        string IdxStr(2, 0);
+	for (int i = 0; i < m; ++i)
+	{
+		string IdxPair(2, 0);
 
-        IdxStr[1] = i;
-        OutsideStuffs.insert(IdxStr);
+		IdxPair[1] = i;
+		OutsideStuffSet.insert(IdxPair);
 
-        IdxStr[0] = n - 1;
-        OutsideStuffs.insert(IdxStr);
-    }
+		IdxPair[0] = n - 1;
+		OutsideStuffSet.insert(IdxPair);
+	}
 
-    for (int j = 1; j < n - 1; ++j)
-    {
-        string IdxStr(2, 0);
-        
-        IdxStr[0] = j;
-        OutsideStuffs.insert(IdxStr);
+	for (int j = 1; j < n - 1; ++j)
+	{
+		string IdxPair(2, 0);
 
-        IdxStr[1] = m - 1;
-        OutsideStuffs.insert(IdxStr);
-    }
+		IdxPair[0] = j;
+		OutsideStuffSet.insert(IdxPair);
 
-    // do something
-    for (const auto& ReqStr : requests)
-    {
-        if (1 == ReqStr.size())
-        {
+		IdxPair[1] = m - 1;
+		OutsideStuffSet.insert(IdxPair);
+	}
 
-        }
-        else
-        {
+	// do something
+	for (const auto& ReqStr : requests)
+	{
+		if (1 == ReqStr.size())
+		{
+			unordered_set<string> Buf;
+			auto Iter = OutsideStuffSet.begin();
 
-        }
-    }
+			for (int k = 0; k < OutsideStuffSet.size(); ++k)
+			{
+				auto& IdxPair = *Iter;
 
-    for (auto IdxStr : OutsideStuffs)
-    {
-        IdxStr[0] += 48;
-        IdxStr[1] += 48;
+				if (ReqStr[0] == storage[IdxPair[0]][IdxPair[1]])
+				{
+					if (IdxPair[0] - 1 < n	&&
+						IdxPair[0] - 1 >= 0 &&
+						OutsideStuffSet.end() == OutsideStuffSet.find({char(IdxPair[0] - 1) , char(IdxPair[1])}))
+						Buf.insert({ char(IdxPair[0] - 1) , char(IdxPair[1]) });
+					if (IdxPair[0] + 1 < n	&&
+						IdxPair[0] + 1 >= 0 &&
+						OutsideStuffSet.end() == OutsideStuffSet.find({ char(IdxPair[0] + 1) , char(IdxPair[1]) }))
+						Buf.insert({ char(IdxPair[0] + 1) , char(IdxPair[1]) });
+					if (IdxPair[1] - 1 < m	&&
+						IdxPair[1] - 1 >= 0 &&
+						OutsideStuffSet.end() == OutsideStuffSet.find({ char(IdxPair[0]) , char(IdxPair[1] - 1) }))
+						Buf.insert({ char(IdxPair[0]) , char(IdxPair[1] - 1) });
+					if (IdxPair[1] + 1 < m	&&
+						IdxPair[1] + 1 >= 0 &&
+						OutsideStuffSet.end() == OutsideStuffSet.find({ char(IdxPair[0]) , char(IdxPair[1] + 1) }))
+						Buf.insert({ char(IdxPair[0]) , char(IdxPair[1] + 1) });
 
-        cout << IdxStr << endl;
-    }
+					Status[IdxPair[0]][IdxPair[1]] = false;
+				}
+				else
+				{
+					Buf.insert(IdxPair);
+				}
 
-    return answer;
+				++Iter;
+			}
+
+			OutsideStuffSet = Buf;
+
+			/*
+			for (auto Iter = OutsideStuffSet.begin(); Iter != OutsideStuffSet.end(); ++Iter)
+				cout << storage[(*Iter)[0]][(*Iter)[1]] << endl;			
+			*/
+		}
+		else
+		{
+
+		}
+	}
+
+	return answer;
 }
