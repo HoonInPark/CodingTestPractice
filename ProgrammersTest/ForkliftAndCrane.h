209759c3a14f6aea7ab57a1228b058869b67965a
HoonInPark <hooninpark@gmail.com>
2025-10-12 18:02:49 +0900

    정답이다! 연금술사!

--- ProgrammersTest/ForkliftAndCrane.h
+++ ProgrammersTest/ForkliftAndCrane.h
@@ -31,36 +31,61 @@ int solution(vector<string> storage, vector<string> requests)
 	* 이 때, 이미 가 본 경로를 어떻게 중복하지 않고 BFS를 실행할지가 관건.
 	*/
 
-	unordered_map<int, bool> Status;
+	unordered_map<int, int> Status;
 	for (int i = 0; i < n * m; ++i)
-		Status.insert(make_pair(i, true));
+		Status.insert(make_pair(i, 0)); // 없고 순회하지 않았으면 0, 없고 순회했으면 -1, 있으면 1, 있는데 외곽에 있다고 순회하면서 확인했으면 2
 
-	unordered_set<int> Outsiders; // 외곽 컨테이너들을 담는 자료형. 라운드마다 갱신됨.
-	stack<int> Path;
+	stack<int> PathStack;
 
 	function<void(int)> PathFinder =
 		[&](int _InInitNum)
 		{
-			Path.push(_InInitNum);
+			PathStack.push(_InInitNum);
 
-			while (false == Path.empty())
+			while (false == PathStack.empty())
 			{
-				int TopNum = Path.top();
-				Path.pop();
+				int TopNum = PathStack.top();
+				PathStack.pop();
 
 				for (const auto Dir : Dirs)
 				{
 					int NxtNum = TopNum + Dir;
 
-					if (NxtNum >= 0		&&
-						NxtNum < n * m	&&			// 만약 해당 정수가 유효한 인덱스이고
-						false == Status[NxtNum])	// 그 자리가 비어 있으면
+					if (NxtNum >= 0 &&
+						NxtNum < n * m)
 					{
-						Path.push(NxtNum);
+						switch (Status[NxtNum])
+						{
+						case 0: // 없고 순회 아직 안했으면
+						{
+							Status[NxtNum] = -1;
+							PathStack.push(NxtNum);
+
+							break;
+						}
+						case 1: // 있고 순회 아직 안했으면
+						{
+							Status[NxtNum] = 2; // 순회했다고 표시하는데, 이는 곧 바깥에 있음을 의미.
+							break;
+						}
+						default:
+							break;
+						}
 					}
 				}
 			}
 		};
+
+	// PathFinder로 모든 순회하지 않은 블록으로부터 출발하여 순회를 시작하고, 그게 다 된 뒤에
+	// 없는데 순회됐다고 표시한 것들을 초기화.
+	// 그런 다음에야 새로 블록을 빼는 행위가 이루어진다. 
+
+	for (int j = 0; j < Status.size(); ++j)
+		if (0 == Status[j])
+			PathFinder(j);
+
+	// 끝나면 순회했다고 표시한 거 원복.
+
 
 	return -1;
 }
