8a4eeb1a189bc0858956401c73d9cedc3b4b247b
HoonInPark <hooninpark@gmail.com>
2025-10-16 20:50:56 +0900

    mod to use memoization

--- ProgrammersTest/RecoverMathod.h
+++ ProgrammersTest/RecoverMathod.h
@@ -7,7 +7,9 @@ using namespace std;
 
 vector<string> solution(vector<string> expressions)
 {
-    vector<vector<int>> Methods(expressions.size(), vector<int>(4)); // basic op in ascii num, 1st num, 2nd num, res num
+    // basic op in ascii num, 1st num, 2nd num, res num
+    // and each num is stored as vector<int>.
+    vector<vector<vector<int>>> Methods(expressions.size(), vector<vector<int>>(4)); 
 
     for (int i = 0; i < expressions.size(); ++i)
     {
@@ -17,40 +19,34 @@ vector<string> solution(vector<string> expressions)
         int IdxOfBasicOp = Ex.find('+', 2) + Ex.find('-', 2) + 1;
         int IdxOfEqual = Ex.find('=', 6);
 
-        Methods[i][0] = Ex[IdxOfBasicOp];
+        Methods[i][0].push_back(Ex[IdxOfBasicOp]);
 
+        Methods[i][1].push_back(Ex[0] - 48);
         if (3 == IdxOfBasicOp) // if 1st num is two digits
-            Methods[i][1] = (Ex[0] - 48) * 10 + (Ex[1] - 48);
-        else
-            Methods[i][1] = Ex[0] - 48;
+            Methods[i][1].push_back(Ex[1] - 48);
 
-        if (5 == IdxOfEqual - IdxOfBasicOp) // if 2nd num is two digits
-            Methods[i][2] = (Ex[IdxOfBasicOp + 2] - 48) * 10 + (Ex[IdxOfBasicOp + 3] - 48);
-        else
-            Methods[i][2] = Ex[IdxOfBasicOp + 2] - 48;
+        Methods[i][2].push_back(Ex[IdxOfBasicOp + 2] - 48);
+        if (5 == IdxOfEqual - IdxOfBasicOp) // if 1st num is two digits
+            Methods[i][2].push_back(Ex[IdxOfBasicOp + 3] - 48);
 
         if ('X' == Ex.back())
         {
-            Methods[i][3] = -1; // if 'X', we mark as -1
+            // if 'X', we mark as -1
+            Methods[i][3].push_back(-1);
         }
         else
         {
             // res num doesn't have limit in digit
-            int ExResNum = 0;
-            int Digit = 1;
-            for (int j = Ex.size() - 1; j >= IdxOfEqual + 2; --j)
-            {
-                ExResNum += (Ex[j] - 48) * Digit;
-                Digit *= 10;
-            }
-
-            Methods[i][3] = ExResNum;
+            for (int j = IdxOfEqual + 2; j < Ex.size(); ++j)
+                Methods[i][3].push_back(Ex[j]);
         }
     }
 
     vector<string> answer(expressions.size());
     for (int NumSys = 2; NumSys < 10; ++NumSys)
     {
+
+
         for (const auto& Method : Methods)
         {
 
