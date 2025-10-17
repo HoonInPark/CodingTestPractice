1a7a8a957108cf90f19ea3de19889e65ed017898
HoonInPark <hooninpark@gmail.com>
2025-10-17 15:40:36 +0900

    almost done...

--- ProgrammersTest/RecoverMathod.h
+++ ProgrammersTest/RecoverMathod.h
@@ -38,18 +38,92 @@ vector<string> solution(vector<string> expressions)
         {
             // res num doesn't have limit in digit
             for (int j = IdxOfEqual + 2; j < Ex.size(); ++j)
-                Methods[i][3].push_back(Ex[j]);
+                Methods[i][3].push_back(Ex[j] - 48);
         }
     }
 
-    vector<string> answer(expressions.size());
+    vector<string> answer;
     for (int NumSys = 2; NumSys < 10; ++NumSys)
     {
+        bool bIsEachDigitFitForNumSys = true;
+        bool bIsEachEqIsValid = true;
 
+        vector<int> MethodIdxsToSolve;
+        vector<vector<int>> MethodNumsToSolve;
 
-        for (const auto& Method : Methods)
+        for (int k = 0; k < Methods.size(); ++k)
         {
+            const auto& Method = Methods[k];
 
+            for (int l = 1; l < 4; ++l)
+            {
+                for (int p = 0; p < Method[l].size(); ++p)
+                {
+                    if (Method[l][p] >= NumSys)
+                    {
+                        bIsEachDigitFitForNumSys = false;
+                        continue;
+                    }
+                }
+            }
+
+            if (false == bIsEachDigitFitForNumSys)
+                break;
+
+            vector<int> Nums(3, 0);
+            for (int l = 1; l < 4; ++l)
+            {
+                int Digit = 1;
+
+                for (int p = Method[l].size() - 1; p >= 0; --p)
+                {
+                    Nums[l - 1] += Method[l][p] * Digit;
+                    Digit *= NumSys;
+                }
+            }
+
+            if (Nums[2] > 0) // if it doesn't have 'X'
+            {
+                // check if equation of basic operation is valid
+                if (Nums[2] != (Nums[0] + Nums[1]))
+                    bIsEachEqIsValid = false;
+            }
+            else // if it has 'X'
+            {
+                MethodIdxsToSolve.push_back(k);
+                MethodNumsToSolve.push_back(Nums);
+            }
+
+            if (false == bIsEachEqIsValid)
+                break;
+        }
+
+        if (true == bIsEachEqIsValid)
+            continue;
+
+        for (int q = 0; q < MethodIdxsToSolve.size(); ++q)
+        {
+            auto& Ex = expressions[q];
+            const auto& Nums = MethodNumsToSolve[q];
+
+            Ex.pop_back();
+
+            const int ResNum = Nums[0] + Nums[1];
+
+            int Digit = NumSys;
+            int DigitNum = 1;
+
+            while (ResNum / Digit != 0)
+            {
+                Digit *= NumSys;
+                ++DigitNum;
+            }
+
+            string StrX;
+            for (int r = 0; r < DigitNum; ++r)
+            {
+                
+            }
         }
     }
 
